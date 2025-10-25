694db28faf350b7219b54d8e97450ca262392eec
Sergio Santos Mendez <80726481+sergiomendez1998@users.noreply.github.com>
2025-10-25 07:49:38 -0600

    Update API_BASE_URL to new backend address

--- src/api/config.js
+++ src/api/config.js
@@ -1,5 +1,5 @@
 
-export const API_BASE_URL = 'http://localhost:8080/api/v1'; // TODO: Cambia a la URL pública del backend en producción, p.ej. 'https://api.tu-dominio.com/api/v1'
+export const API_BASE_URL = 'http://93.127.139.74:8080/EVENTOS/api/v1'; // TODO: Cambia a la URL pública del backend en producción, p.ej. 'https://api.tu-dominio.com/api/v1'
 
 
 export function joinUrl(path) {
